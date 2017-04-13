0eee80cb6fade545e9fc6fd01d9b965505a2b354
Aykut Gedik <aykutged@gmail.com>
2017-04-13 14:46:46 -0400

    initial files

---
+++ HttpRequest/RequestViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HttpRequest
+//
+//  Created by Aykut Gedik on 4/13/17.
+//
+//
+
+import UIKit
+
+class RequestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
